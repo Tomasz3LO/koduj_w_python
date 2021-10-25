2530a9c6f1a9a3616b0b627a13704491b14fd1ee
Tomasz Wysocki <tomasz.wysocki@trzecie.edu.pl>
2021-10-25 18:53:57 +0200

    lekcja_01_poczatek
    zdefinowalismy rozdzielczosc ekranu. Zadeklarowalismy zmienne i classy w grze.

--- program_00.py
+++ program_00.py
@@ -1,2 +1,19 @@
-WIDTH = 1280
-HEIGHT = 720
+WIDTH = 1280 # szerokość okna
+HEIGHT = 720 # wysokość okna
+
+# definicje klas
+
+class Game:
+   # na razie nie robimy nice
+  pass
+
+class Key:
+    # na razie nie robimy nice
+  pass
+
+# podstawowe zmienne
+background_active = "corridor-01.jpg"
+background_position = (0,0)
+
+def update():
+    screen.blit(background_active,background_position)
